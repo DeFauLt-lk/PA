91fe2cf870c4b1f7552255103e416facc873c4e5
DeFauLt-lk <58408351+DeFauLt-lk@users.noreply.github.com>
2019-12-02 17:06:31 -0300

    exercícios de P.A
    Lista 6 dos exercícios da profº Aline

---
+++ lista6/src/lista6/exercicio2.java
@@ -0,0 +1,38 @@
+package lista6;
+
+import java.util.*;
+
+public class exercicio2 {
+
+	public static void main(String[] args) {
+		Scanner ler= new Scanner(System.in);
+	final int Const=10;
+	int i, a[], num[];
+	
+	a=new int [Const];
+	num= new int[Const];	
+	
+	for (i=0;i<Const;i++) {
+		System.out.println("entre com  "+(i+1)+"° que deseja");
+		a[i]= ler.nextInt();
+		num[i]=0;
+		
+		for (int j = 1; j <=a[i]; j++) {
+			if(a[i]%j==0) {
+				num[i]++;
+			}
+		}
+	}
+	for(i=0; i<Const;i++) {
+		if(num[i]==2){
+			
+		
+		System.out.println(a[i]+"  é um número primo!");
+		}else {
+		
+		System.out.println(a[i]+"  Não é primo");
+		}
+	}
+	
+	}
+}
---
+++ lista6/src/lista6/exercicio4.java
@@ -0,0 +1,54 @@
+package lista6;
+
+import java.util.Scanner;
+
+public class exercicio4 {
+
+	public static void main(String[] args) {
+		 Scanner ler= new Scanner (System.in);
+		 
+		 final int Const=10;
+		 
+		 int aux,i,menor,a[];
+		 
+	
+		 
+		 a=new int [Const];
+		 
+		 ;
+		 for(i=0;i<Const;i++) {
+			 System.out.println("Digite o "+(i+1)+" valor");
+			 a[i]= ler.nextInt();			 
+		 }	 
+		 
+		 aux=a[0];
+		 menor=a[0];
+		 	
+		 // Maior valor
+		 for(i=1;i<Const;i++) {
+			
+			 if(aux<a[i]){
+				 aux= a[i];
+			 }
+		 }	 
+			 
+		 //Menor valor
+		 
+		 for(i=1;i<Const;i++) {
+					
+				 if(menor>a[i]){
+					 menor= a[i];
+				}
+		 }	
+		 
+		 for(i=0;i<Const;i++) {
+			 System.out.println(a[i]+"/n ");
+		 }		
+			
+		 System.out.println(aux+" maior valor");
+		 
+		 System.out.println(menor+ " menor valor ");
+			
+
+	}
+}
